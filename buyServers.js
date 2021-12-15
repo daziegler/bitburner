1fe9cbd149df4bb50c23afefe08a7df8ad6e2306
Daniel Ziegler <daziegler@users.noreply.github.com>
2021-12-15 18:05:57 +0100

    Create buyServers.js

---
+++ buyServers.js
@@ -0,0 +1,33 @@
+function buyServers(args = []) {
+    // Default, we buy until we hit the limit
+    var numberOfServers = getPurchasedServerLimit() - getPurchasedServers().length;
+    if (args.length > 0 && args[0] < numberOfServers) {
+        numberOfServers = args[0];
+    }
+
+    var serverNameTemplate = 'hackserv-';
+    if (args.length > 1) {
+        serverNameTemplate = args[1];
+    }
+
+    var player = getPlayer();
+
+    var maxAffordableRam = 0;
+    var purchasedServerMaxRam = getPurchasedServerMaxRam();
+    for (var r = 0; r * r <= (purchasedServerMaxRam/1024); r++) {
+        var ram = r * r * 1024
+        if (getPurchasedServerCost(numberOfServers * ram) < player.money) {
+            maxAffordableRam = ram;
+            continue;
+        }
+
+        break;
+    }
+
+    // TODO: check if we already own the server, and if this would be an upgrade
+    for (var i = 0; i < numberOfServers; i++) {
+        purchaseServer(serverNameTemplate + i, maxAffordableRam);
+    }
+
+    spawn('validateServers.script');
+}
