f9fe70a9f7a7bb9521008ea887c7db7423a550a1
Daniel Ziegler <daziegler@users.noreply.github.com>
2021-12-15 18:43:49 +0100

    Update upgradeServers.js

--- upgradeServers.js
+++ upgradeServers.js
@@ -1,19 +1,22 @@
 // Updates all already owned servers
-// If the maximum amount of ram is not affordable, one server will be upgraded to the highest capacity the player can afford
 function upgradeServers(args = []) {
     var player = getPlayer();
     var ownedServers = getPurchasedServers();
     var purchasedServerMaxRam = getPurchasedServerMaxRam();
     var maxAffordableRam = 0;
 
-    for (var r = 1; r * r <= purchasedServerMaxRam; r++) {
-        var ram = r * r;
-        if (getPurchasedServerCost(ownedServers.length * ram) < player.money) {
+    for (var r = 1; Math.pow(2, r) <= purchasedServerMaxRam; r++) {
+        var ram = Math.pow(2, r);
+        if ((getPurchasedServerCost(ownedServers.length) * ram) < player.money) {
             maxAffordableRam = ram;
             continue;
         }
 
         break;
+    }
+
+    if (maxAffordableRam === 0) {
+        exit();
     }
 
     var requiresUpdate = false;
