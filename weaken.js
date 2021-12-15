5bb20a8fc4e24d26c00e527f878afadd43f9d515
Daniel Ziegler <daziegler@users.noreply.github.com>
2021-12-15 16:24:15 +0100

    Add weaken.js Script

---
+++ weaken.js
@@ -0,0 +1,22 @@
+    var serversToWeaken = read('validServers.txt').split(',');
+    var serversToIgnore = [];
+    while (serversToIgnore.length < serversToWeaken.length) {
+        for (var i = 0; i < serversToWeaken.length; i++) {
+            var serverToWeaken = serversToWeaken[i];
+            if (serversToIgnore.includes(serverToWeaken)) {
+                continue;
+            }
+
+            if (hasRootAccess(serverToWeaken) === false) {
+                serversToIgnore.push(serverToWeaken);
+                continue;
+            }
+
+            if (getServerSecurityLevel(serverToWeaken) > getServerMinSecurityLevel(serverToWeaken)) {
+                weaken(serverToWeaken);
+                continue;
+            }
+
+            serversToIgnore.push(serverToWeaken);
+        }
+    }
