6e0c0ccd002dc79a562bf0dc3aa9f3ca07c2a5b4
Daniel Ziegler <daziegler@users.noreply.github.com>
2021-12-19 14:51:47 +0100

    Update hacknetMaxout.js

--- hacknetMaxout.js
+++ hacknetMaxout.js
@@ -4,18 +4,18 @@ export async function main(ns) {
     const waitTime = 1000 * 60 * 10;
 
     const ownedNodes = ns.hacknet.numNodes();
-    for(let n = 0; n < ownedNodes; n++) {
+    for (let n = 0; n < ownedNodes; n++) {
         if (ns.hacknet.getNodeStats(n).level !== 200) {
             while (ns.hacknet.getNodeStats(n).level < 200) {
                 let lvlCost = ns.hacknet.getLevelUpgradeCost(n, 1);
                 while (ns.getPlayer().money < lvlCost) {
-                    ns.tprintf(
+                    ns.print(ns.sprintf(
                         'Need $%d for the upgrade of node %d from lvl %d. Have $%d',
                         lvlCost,
                         n,
                         ns.hacknet.getNodeStats(n).level,
                         ns.getPlayer().money
-                    );
+                    ));
                     await ns.sleep(waitTime);
                 }
                 ns.hacknet.upgradeLevel(n, 1);
@@ -25,13 +25,13 @@ export async function main(ns) {
             while (ns.hacknet.getNodeStats(n).ram < 64) {
                 let ramCost = ns.hacknet.getRamUpgradeCost(n, 1);
                 while (ns.getPlayer().money < ramCost) {
-                    ns.tprintf(
+                    ns.print(ns.sprintf(
                         'Need $%d for the upgrade of node %d from ram %d. Have $%d',
                         ramCost,
                         n,
                         ns.hacknet.getNodeStats(n).ram,
                         ns.getPlayer().money
-                    );
+                    ));
                     await ns.sleep(waitTime);
                 }
                 ns.hacknet.upgradeRam(n, 1);
@@ -41,17 +41,17 @@ export async function main(ns) {
             while (ns.hacknet.getNodeStats(n).cores < 16) {
                 let coreCost = ns.hacknet.getCoreUpgradeCost(n, 1);
                 while (ns.getPlayer().money < coreCost) {
-                    ns.tprintf(
+                    ns.print(ns.sprintf(
                         'Need $%d for the upgrade of node %d from core %d. Have $%d',
                         coreCost,
                         n,
-                        ns.hacknet.getNodeStats(n).ram,
+                        ns.hacknet.getNodeStats(n).cores,
                         ns.getPlayer().money
-                    );
+                    ));
                     await ns.sleep(waitTime);
                 }
                 ns.hacknet.upgradeCore(n, 1);
             }
         }
-    }    
+    }
 }
