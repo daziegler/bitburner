e6757cf651ecf78819bce8a1d22c2bfc24071b2a
Daniel Ziegler <daziegler@users.noreply.github.com>
2021-12-20 11:41:47 +0100

    Update upgradeServers.js

--- upgradeServers.js
+++ upgradeServers.js
@@ -1,13 +1,12 @@
-// Updates all already owned servers
-function upgradeServers(args = []) {
-    var player = getPlayer();
-    var ownedServers = getPurchasedServers();
-    var purchasedServerMaxRam = getPurchasedServerMaxRam();
-    var maxAffordableRam = 0;
+/** @param {NS} ns **/
+export async function main(ns) {
+    let ownedServers = ns.getPurchasedServers();
+    let purchasedServerMaxRam = ns.getPurchasedServerMaxRam();
+    let maxAffordableRam = 0;
 
-    for (var r = 1; Math.pow(2, r) <= purchasedServerMaxRam; r++) {
-        var ram = Math.pow(2, r);
-        if ((getPurchasedServerCost(ownedServers.length) * ram) < player.money) {
+    for (let r = 1; Math.pow(2, r) <= purchasedServerMaxRam; r++) {
+        let ram = Math.pow(2, r);
+        if ((ns.getPurchasedServerCost(ram) * ownedServers.length) < ns.getPlayer().money) {
             maxAffordableRam = ram;
             continue;
         }
@@ -16,24 +15,24 @@ function upgradeServers(args = []) {
     }
 
     if (maxAffordableRam === 0) {
-        exit();
+        ns.exit();
     }
 
-    var requiresUpdate = false;
-    for (var i = 0; i < ownedServers.length; i++) {
-        var server = ownedServers[i];
-        var serverMaxRam = getServerMaxRam(server);
+    let requiresUpdate = false;
+    for (let i = 0; i < ownedServers.length; i++) {
+        let server = ownedServers[i];
+        let serverMaxRam = ns.getServerMaxRam(server);
         if (serverMaxRam >= maxAffordableRam) {
             continue;
         }
 
-        killall(newServerName);
-        deleteServer(server);
-        purchaseServer(server, maxAffordableRam);
+        ns.killall(server);
+        ns.deleteServer(server);
+        ns.purchaseServer(server, maxAffordableRam);
         requiresUpdate = true;
     }
 
     if (requiresUpdate) {
-        spawn('validateServers.script');
+        ns.spawn('validate.ns');
     }
 }
