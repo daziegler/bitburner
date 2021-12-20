fa878511850fbf49828939e59499614f262fffc4
Daniel Ziegler <daziegler@users.noreply.github.com>
2021-12-20 10:29:11 +0100

    Update weaken.js

--- weaken.js
+++ weaken.js
@@ -1,21 +1,18 @@
-// Function will weaken servers from the validServers.txt if possible
-function weakenScript(args = []) {
-    var serversToWeaken = read('validServers.txt').split(',');
-    var serversToIgnore = [];
+// Function will weaken servers from the validServersWithoutOwn.txt if possible
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    let serversToWeaken = await ns.read('validServersWithoutOwn.txt').split(',');
+    let serversToIgnore = [];
     while (serversToIgnore.length < serversToWeaken.length) {
-        for (var i = 0; i < serversToWeaken.length; i++) {
-            var serverToWeaken = serversToWeaken[i];
+        for (let i = 0; i < serversToWeaken.length; i++) {
+            let serverToWeaken = serversToWeaken[i];
             if (serversToIgnore.includes(serverToWeaken)) {
                 continue;
             }
 
-            if (hasRootAccess(serverToWeaken) === false) {
-                serversToIgnore.push(serverToWeaken);
-                continue;
-            }
-
-            if (getServerSecurityLevel(serverToWeaken) > getServerMinSecurityLevel(serverToWeaken)) {
-                weaken(serverToWeaken);
+            if (ns.getServerSecurityLevel(serverToWeaken) > ns.getServerMinSecurityLevel(serverToWeaken)) {
+                await ns.weaken(serverToWeaken);
                 continue;
             }
 
