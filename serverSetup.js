15644a5d404a5a5ca1adb9b051f4add1bb900600
Daniel Ziegler <daziegler@users.noreply.github.com>
2021-12-20 10:20:47 +0100

    Update serverSetup.js

--- serverSetup.js
+++ serverSetup.js
@@ -1,30 +1,36 @@
-// This script copies the relevant scrupts to the valid servers found in the validServer.txt
-// It will run the scripts at their highes possible thread count after killing all processes, that were still running on the remote servers.
-function serverSetup(args = []) {
-    var serversToSetup = read('validServers.txt').split(',');
+// It will run the scripts at their highest possible thread count after killing all processes, that were still running on the remote servers.
+/** @param {NS} ns **/
+export async function main(ns) {
+    let serversToSetup = ns.read('validServers.txt').split(',');
 
-    var filesToCopy = [
+    let filesToCopy = [
+        'trainHack.ns',
         'hack.script',
         'weaken.script',
+        'grow.script',
+        'growLight.script',
         'validServers.txt',
         'validServersWithoutOwn.txt'
     ];
 
-    var scriptsToRun = [
-        {name: 'hack.script', ramUse: getScriptRam('hack.script'), args: 1000000},
-        {name: 'weaken.script', ramUse: getScriptRam('weaken.script'), args: ''},
+    let scriptsToRun = [
+        {name: 'hack.script', ramUse: ns.getScriptRam('hack.script'), args: 3000000},
+        {name: 'weaken.script', ramUse: ns.getScriptRam('weaken.script'), args: ''},
+        {name: 'grow.script', ramUse: ns.getScriptRam('grow.script'), args: ''},
+        {name: 'growLight.script', ramUse: ns.getScriptRam('growLight.script'), args: ''},
+        {name: 'trainHack.ns', ramUse: getScriptRam('trainHack.ns'), args: ''},
     ];
 
-    for (var i = 0; i < serversToSetup.length; i++) {
-        var serverName = serversToSetup[i];
-        var maxRam = getServerMaxRam(serverName);
-        killall(serverName);
-        scp(filesToCopy, serverName);
+    for (let i = 0; i < serversToSetup.length; i++) {
+        let serverName = serversToSetup[i];
+        let maxRam = ns.getServerMaxRam(serverName);
+        ns.killall(serverName);
+        await ns.scp(filesToCopy, serverName);
 
-        for (var sr = 0; sr < scriptsToRun.length; sr++) {
-            var script = scriptsToRun[sr];
-            var usedRam = getServerUsedRam(serverName);
-            var threads = 1;
+        for (let sr = 0; sr < scriptsToRun.length; sr++) {
+            let script = scriptsToRun[sr];
+            let usedRam = ns.getServerUsedRam(serverName);
+            let threads = 1;
             if (script.ramUse > 0) {
                 threads = Math.floor((maxRam - usedRam) / script.ramUse);
             }
@@ -32,7 +38,7 @@ function serverSetup(args = []) {
             if (threads <= 0) {
                 continue;
             }
-            exec(script.name, serverName, threads, script.args);
+            ns.exec(script.name, serverName, threads, script.args);
         }
     }
 }
