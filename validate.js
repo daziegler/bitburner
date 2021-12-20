dd3aa3a0bf0e563ce767d17719a61f6f5cb35c3c
Daniel Ziegler <daziegler@users.noreply.github.com>
2021-12-20 08:22:11 +0100

    Update validate.js

--- validate.js
+++ validate.js
@@ -5,72 +5,89 @@
 // - invalidServers.txt contains all servers, that are too high level, have to many required ports etc.
 function validate(args = []) {
     var serversToHack = scan("home");
-	var validatedServers = [];
-	var ignoredServers = [];
-	var hackingLevel = getHackingLevel();
-	var availablePortScripts = 0;
+    var validatedServers = [];
+    var ignoredServers = [];
+    var hackingLevel = getHackingLevel();
+    var availablePortScripts = 4;
 
-	// Validation
-	while ((ignoredServers.length + validatedServers.length) < serversToHack.length) {
-		for (var v = 0; v < serversToHack.length; v++) {
-			var serverToValidate = serversToHack[v];
-			if (ignoredServers.includes(serverToValidate) || validatedServers.includes(serverToValidate)) {
-				continue;
-			}
+    // Validation
+    while ((ignoredServers.length + validatedServers.length) < serversToHack.length) {
+        for (var v = 0; v < serversToHack.length; v++) {
+            var serverToValidate = serversToHack[v];
+            if (ignoredServers.includes(serverToValidate) || validatedServers.includes(serverToValidate)) {
+                continue;
+            }
 
-			// Validation
-			if (validatedServers.includes(serverToValidate) === false) {
-				if (serverToValidate === 'home') {
-					ignoredServers.push(serverToValidate);
-					continue;
-				}
-				if (serverExists(serverToValidate) === false) {
-					ignoredServers.push(serverToValidate);
-					continue;
-				}
-				if (getServerRequiredHackingLevel(serverToValidate) > hackingLevel) {
-					ignoredServers.push(serverToValidate);
-					continue;
-				}
-			}
+            // Validation
+            if (validatedServers.includes(serverToValidate) === false) {
+                if (serverToValidate === 'home') {
+                    ignoredServers.push(serverToValidate);
+                    continue;
+                }
+                if (serverExists(serverToValidate) === false) {
+                    ignoredServers.push(serverToValidate);
+                    continue;
+                }
+                if (getServerRequiredHackingLevel(serverToValidate) > hackingLevel) {
+                    ignoredServers.push(serverToValidate);
+                    continue;
+                }
+            }
 
-			if (hasRootAccess(serverToValidate) === false) {
-				var requiredPortAmountForServer = getServerNumPortsRequired(serverToValidate);
-				if (requiredPortAmountForServer > availablePortScripts) {
-					ignoredServers.push(serverToValidate);
-					continue;
-				}
+            if (hasRootAccess(serverToValidate) === false) {
+                var requiredPortAmountForServer = getServerNumPortsRequired(serverToValidate);
+                if (requiredPortAmountForServer > availablePortScripts) {
+                    ignoredServers.push(serverToValidate);
+                    continue;
+                }
 
-				if (requiredPortAmountForServer > 0) {
-					brutessh(serverToValidate);
-				}
+                if (requiredPortAmountForServer > 0) {
+                    brutessh(serverToValidate);
+                }
 
-				nuke(serverToValidate);
-			}
+                if (requiredPortAmountForServer > 1) {
+                    ftpcrack(serverToValidate);
+                }
 
-			validatedServers.push(serverToValidate);
+                if (requiredPortAmountForServer > 2) {
+                    relaysmtp(serverToValidate);
+                }
 
-			var newTargets = scan(serverToValidate);
-			for (var x = 0; x < newTargets.length; x++) {
-				if (serversToHack.includes(newTargets[x])) {
-					continue;
-				}
+                if (requiredPortAmountForServer > 3) {
+                    httpworm(serverToValidate);
+                }
 
-				serversToHack.push(newTargets[x]);
-			}
-		}
-	}
+                if (requiredPortAmountForServer > 4) {
+                    sqlinject(serverToValidate);
+                }
 
-	write('validServers.txt', validatedServers, 'w');
-	write('invalidServers.txt', ignoredServers, 'w');
+                nuke(serverToValidate);
+            }
 
-	var ownServers = getPurchasedServers();
-	ownServers.push('home');
+            validatedServers.push(serverToValidate);
 
-	for (var v = 0; v < validatedServers.length; v++) {
-		if (ownServers.includes(validatedServers[v])) {
-			validatedServers.splice(v, 1);
-		}
-	}
-	write('validServersWithoutOwn.txt', validatedServers, 'w');
+            var newTargets = scan(serverToValidate);
+            for (var x = 0; x < newTargets.length; x++) {
+                if (serversToHack.includes(newTargets[x])) {
+                    continue;
+                }
+
+                serversToHack.push(newTargets[x]);
+            }
+        }
+    }
+
+    write('validServers.txt', validatedServers, 'w');
+    write('invalidServers.txt', ignoredServers, 'w');
+
+    var ownServers = getPurchasedServers();
+    var validatedServersWithoutOwn = [];
+    for (var v = 0; v < validatedServers.length; v++) {
+        if (ownServers.includes(validatedServers[v])) {
+            continue;
+        }
+        validatedServersWithoutOwn.push(validatedServers[v]);
+    }
+
+    write('validServersWithoutOwn.txt', validatedServersWithoutOwn, 'w');
 }
